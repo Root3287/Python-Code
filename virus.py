3c6880ecafbe0afc2d54f39e16b236c9b032a128
Timothy Gibbons <Root3287@users.noreply.github.com>
2016-03-17 14:37:56 -0500

    more clones

--- virus.py
+++ virus.py
@@ -4,17 +4,18 @@ import os
 import time
 
 payload = open('payload.txt', 'a+')
-for i in range(1,99999999999):
-	payload.write("I'm the Creeper. Catch me if you can!\n")
-	time.sleep(60)
-print("I'm the Creeper. Catch me if you can!");
-for i in range(1,99999999999):
-	payload.write("I'm the Creeper. Catch me if you can!\n")
-	time.sleep(60)
-print("I'm the Creeper. Catch me if you can!");
-for i in range(1,99999999999):
-	payload.write("I'm the Creeper. Catch me if you can!\n")
-	time.sleep(60)
+for j in range(1,99999999999):
+	for i in range(1,99999999999):
+		payload.write("I'm the Creeper. Catch me if you can!\n")
+		time.sleep(60)
+	print("I'm the Creeper. Catch me if you can!");
+	for i in range(1,99999999999):
+		payload.write("I'm the Creeper. Catch me if you can!\n")
+		time.sleep(60)
+	print("I'm the Creeper. Catch me if you can!");
+	for i in range(1,99999999999):
+		payload.write("I'm the Creeper. Catch me if you can!\n")
+		time.sleep(60)
 # os.system(r"copy virus.py clone")
 print("I'm the Creeper. Catch me if you can!");
 payload.close()
